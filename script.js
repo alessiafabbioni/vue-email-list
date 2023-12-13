d909f7d37ab72c87e32303fae036f6b2c2e5a337
alessiafabbioni <alessiafabbioni@gmail.com>
2023-12-13 16:59:25 +0100

    email.push

--- script.js
+++ script.js
@@ -22,7 +22,14 @@ createApp({
     },
 
     mounted() {
-        
-      
+        for (let i = 0; i <= 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(response => {
+                        const result = response.data.response;
+                        this.emails.push(result);
+                        console.log(result);
+                    });
+            }
+        }
     }
-}).mount('#app')
+).mount('#app')
